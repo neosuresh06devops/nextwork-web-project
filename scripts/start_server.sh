544efeb1288983c7a864834d00ca042fffe2f62f
neosuresh06devops <64422887+neosuresh06devops@users.noreply.github.com>
2025-11-18 06:44:38 +0530

    Update start_server.sh

--- scripts/start_server.sh
+++ scripts/start_server.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-sudo systemctl start tomcat9.service
-sudo systemctl enable tomcat9.service
+sudo systemctl start tomcat.service
+sudo systemctl enable tomcat.service
 sudo systemctl start httpd.service
 sudo systemctl enable httpd.service
